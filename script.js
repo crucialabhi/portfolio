ed6e9e89f697a755aa4219e44d3da3f76e821ebd
Abhishek Mishra <121038202+crucialabhi@users.noreply.github.com>
2024-03-26 19:21:43 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+var tablinks =document.getElementsByClassName("tab-links");
+var tabcontents =document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab"); 
+}
+var sidemenu = document.getElementById("sidemenu");
+    function openmenu(){
+        sidemenu.style.right ="0";
+    }
+    function closemenu(){
+        sidemenu.style.right="-200px";
+    }
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz6C11b9nGatwSRyUYsixuuFKeFTxr_yIeycjlC_hVc3klcHX1W_V2UOA4l9gZJ8ofTbw/exec'
+    const form = document.forms['submit-to-google-sheet'];
+    const msg =document.getElementById("msg");
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML="Message sent Successfully";
+            setTimeout(function(){
+                msg.innerHTML="";
+            },2000)
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
